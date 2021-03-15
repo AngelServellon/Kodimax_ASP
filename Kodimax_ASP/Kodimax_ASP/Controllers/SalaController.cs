1287c98ab2fe5423d878a2ab52ade3d38d97e340
Angel Servellon <maritzafunes098@outlook.es>
2021-03-14 18:07:18 -0600

    Creation of functionality to edit the cinema rooms

---
+++ Kodimax_ASP/Kodimax_ASP/Controllers/SalaController.cs
@@ -0,0 +1,63 @@
+﻿using Kodimax_ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kodimax_ASP.Controllers
+{
+    public class SalaController : Controller
+    {
+        // GET: Sala
+        public ActionResult Index()
+        {
+            KodimaxContext db = new KodimaxContext();
+            return View(db.Sala.ToList());
+        }
+
+        //Editar sala
+        public ActionResult EditarSala(int id)
+        {
+            try
+            {
+                using (var db = new KodimaxContext())
+                {
+                    Sala sala = db.Sala.Find(id);
+                    return View(sala);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - " + ex.Message);
+                return View();
+            }
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditarSala(Sala s)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new KodimaxContext())
+                {
+                    Sala sala = db.Sala.Find(s.Id_Sala);
+                    sala.Nombre = s.Nombre;
+                    sala.Precio = s.Precio;
+                    sala.Asientos = s.Asientos;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - " + ex.Message);
+                return View();
+            }
+        }
+    }
+}
---
+++ Kodimax_ASP/Kodimax_ASP/Models/GolosinaCE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kodimax_ASP.Models
+{
+    public class GolosinaCE
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Tipo { get; set; }
+        [Required]
+        public double Precio { get; set; }
+    }
+    [MetadataType(typeof(GolosinaCE))]
+    public partial class Golosina
+    {
+
+    }
+}
