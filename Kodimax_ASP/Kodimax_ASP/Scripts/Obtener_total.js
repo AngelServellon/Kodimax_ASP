5e88f68ab3b28ac62a2b1ce53b39c379ab47fd99
Angel Servellon <maritzafunes098@outlook.es>
2021-07-03 20:35:31 -0600

    Taking out the autocomplete function from the text fields and aedition of some details

--- Kodimax_ASP/Kodimax_ASP/Scripts/Obtener_total.js
+++ Kodimax_ASP/Kodimax_ASP/Scripts/Obtener_total.js
@@ -10,17 +10,6 @@ $(document).ready(function () {
         var cambio = pagar - total;
         var cambio_redondeado = cambio.toFixed(2);
 
-        //ticket.append('<h2>KODIMAX</h2><br />\
-        //        <span><strong>Id_Empleado:</strong>@Html.DisplayFor(model => model.id_empleado) <strong>Empleado: </strong></span > <br />\
-        //        <p>@Html.DisplayFor(model => model.Fecha)</p><br />\
-        //        <p><strong>SALA: </strong></p><br />\
-        //        <p>@Html.DisplayNameFor(model => model.Cantidad) x $</p>\
-        //        <p>         @Html.DisplayFor(model => model.SubTotal)</p>\
-        //        <p>----------------------------------------------</p>\
-        //        <p>Subtotal         @Html.DisplayFor(model => model.SubTotal)</p>\
-        //        <p>Impuesto         @Html.DisplayFor(model => model.Tax)</p><br />\
-        //        <p>TOTAL            @Html.DisplayNameFor(model => model.Total)</p>');
-
         if (pagar > total) {
             $('.texto').text("Su cambio es: $" + cambio_redondeado + ", gracias por comprar en KODIMAX");
 
